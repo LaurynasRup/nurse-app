d69ba8a4ce0c05192fe46f7c5b3873efb97c66a2
Laurynas Rupainis <laurynas@thisisbliss.com>
2024-10-19 17:23:59 +0100

    Update react native

---
+++ client/app/(tabs)/index.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function Index() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    async function prepare() {
+      setTimeout(() => {
+        setAppIsReady(true);
+      }, 5);
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>SplashScreen Demo! 👋</Text>
+      {/* <Entypo name="rocket" size={30} /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
